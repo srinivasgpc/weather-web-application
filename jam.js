1182450dbee107075ba2043161d6737375f1f247
Srinivas <srinivasgpc@users.noreply.github.com>
2018-05-19 16:30:25 +0530

    Add files via upload

---
+++ jam.js
@@ -0,0 +1,6 @@
+<ScrollView style horizontal={true}>
+            <Image style={{flex:1, height:null,width:null}}
+                    source= {require('./assets/profile1.png')}></Image>
+                    <Image style={{flex:1, height:null,width:null}}
+                    source= {require('./assets/profile1.png')}></Image>
+                    </ScrollView>
